a82bd8bca1ae42f0d0cbc636497dda5c6cdaecaf
nbr41to <nbr.41to@gmail.com>
2021-04-24 17:49:40 +0900

    init

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,9 @@
-import InputForm from "./InputForm";
-import ItemList from "./ItemList";
-
 const App = () => {
   // JavaScript書く場所
-  const todos = ['パスタ作る', 'PRの修正', 'mtg'];
-
-  const add = () => {
-    todos.push('モンハンやる');
-    console.log(todos);
-  };
-
   return (
     // JSX...jsを使う場合は{}で囲う
     <div className="App">
-      <h1>ToDo App</h1>
-      <InputForm add={add} />
-      <ItemList todos={todos} />
+      <h1>hello</h1>
     </div>
   );
 };
