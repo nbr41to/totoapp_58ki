4e9fc586de7d2ec577d9f829a56f5dae15e2ef2d
nbr41to <nbr.41to@gmail.com>
2021-05-01 13:20:46 +0900

    Revert "2021/05/01"
    This reverts commit 8f27eb0be913ec98a3d31067f8bdc4615cde4af2.

--- src/InputForm.jsx
+++ src/InputForm.jsx
@@ -1,10 +1,14 @@
+import React from 'react';
 
 const InputForm = (props) => {
+  const [inputText, setInputText] = React.useState('');
+  console.log(inputText);
+
   return (
     <div>
       <label>やること:</label>
-      <input type='text' />
-      <button onClick={props.add}>追加</button>
+      <input onChange={(e) => setInputText(e.target.value)} type='text' />
+      <button onClick={() => props.addTodo(inputText)}>追加</button>
     </div>
   );
 };
